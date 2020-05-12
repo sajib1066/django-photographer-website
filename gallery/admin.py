ba42373471964d2ac6b664de46fe046e5a0d5727
sajib1066 <sajib1066@gmail.com>
2020-05-12 10:14:58 +0600

    customize admin

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,5 +1,15 @@
 from django.contrib import admin
 from .models import Category, Gallery
 
-admin.site.register(Category)
-admin.site.register(Gallery)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_delete', 'author', 'date']
+    list_filter = ['name', 'is_delete', 'author']
+    search_fields = ['name']
+
+class GalleryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'is_delete', 'author', 'date']
+    list_filter = ['title', 'category', 'is_delete', 'author']
+    search_fields = ['title']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Gallery, GalleryAdmin)
